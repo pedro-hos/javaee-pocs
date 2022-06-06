9f4178f1076714c02d56dc56dc2fac873bd16235
Pedro Hos <pedro-hos@outlook.com>
2022-06-06 17:56:57 -0300

    domain example

--- security/elytron/ejb-security-jaas/src/main/java/org/jboss/RemoteClient.java
+++ security/elytron/ejb-security-jaas/src/main/java/org/jboss/RemoteClient.java
@@ -15,9 +15,8 @@ public class RemoteClient {
     public static void main(String[] args) throws Exception {
 
         final Hashtable<String, String> jndiProperties = new Hashtable<>();
-        // jndiProperties.put(Context.URL_PKG_PREFIXES, "org.jboss.ejb.client.naming");
         jndiProperties.put(Context.INITIAL_CONTEXT_FACTORY, "org.wildfly.naming.client.WildFlyInitialContextFactory");
-        jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8080");
+        jndiProperties.put(Context.PROVIDER_URL, "remote+http://localhost:8180,remote+http://localhost:8280,remote+http://localhost:8380,remote+http://localhost:8480,remote+http://localhost:8580");
         final Context context = new InitialContext(jndiProperties);
 
         SecuredEJBRemote reference = (SecuredEJBRemote) context.lookup("ejb:/ejb-security-jaas/SecuredEJB!" + SecuredEJBRemote.class.getName());
